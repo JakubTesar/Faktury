807a6793978b086cc94cd76b3413d2944f68d197
JakubTesar <jakub.tesar@educanet.cz>
2021-02-09 12:48:58 +0100

    jau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("seznam-spolecnosti.dat"));
+            String SeznaSpolecnosti = br.readLine();
+            String content = "";
+            String[] SeznamSpolecnostipole ={""};
+
+            while (SeznaSpolecnosti != null){
+                content += SeznaSpolecnosti;
+                SeznaSpolecnosti = br.readLine();
+            }
+            while (content != null){
+                SeznamSpolecnostipole = content.split(":");
+            }
+            for (int i = 0; i < SeznamSpolecnostipole.length; i++) {
+                System.out.println(SeznamSpolecnostipole[i]);
+            }
+
+            br.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
